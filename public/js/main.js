e297bca81e0ca0083351e5c9bb2cced1149d2eb0
Geng <snggeng@gmail.com>
2017-06-14 12:00:51 +0800

    commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,32 @@
 $(document).ready(function () {
-  function initMap () {
-    var uluru = {lat: -25.363, lng: 131.044}
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: uluru
-    })
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map
-    })
-  }
+
 })
+
+function locationSuccess (position) {
+  console.log('Location success')
+  var current = {lat: position.coords.latitude, lng: position.coords.longitude}
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 17,
+    center: current
+  })
+
+  var marker = new google.maps.Marker({
+    position: current,
+    map: map
+  })
+}
+
+function locationError () {
+  console.log('Cound not get location')
+}
+
+function initMap () {
+  navigator.geolocation.getCurrentPosition(locationSuccess, locationError)
+
+  /*
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+  */
+}
